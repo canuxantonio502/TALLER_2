294420364e38a4ed79aef1d249d2d0f77fa37a7a
canuxantonio502 <canuxantonio77@gmail.com>
2026-05-09 16:13:38 -0600

    feat: :sparkles: Menu button added
    A menu button added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 import { getPopularMovies } from "./api.js"
 import { renderMovies } from "./ui.js"
 
+
 async function init(){
     const movies = await getPopularMovies()
     renderMovies(movies)
 }
 
 init()
+
+const menuBtn = document.querySelector(".menu-btn");
+const sidebar = document.querySelector(".sidebar");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+})
